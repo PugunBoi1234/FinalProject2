c8a848a3bb9c036f85da9ec9afb9da48e33d92ae
PugunBoi1234 <stayusaengcanthr@gmail.com>
2025-03-08 05:28:16 +0700

    Update

--- chocolate.js
+++ chocolate.js
@@ -1,19 +1,52 @@
-let chocolatemode = localStorage.getItem('chocolatemode')
+// We make cookies !!! 🍪🍪🍪🍪🍪🍪
+// We also have a theme switcher for the chocolate mode 🍫🍫🍫🍫🍫🍫
+
+let chocolatemode = getCookie('chocolatemode')
+
+let StrawberryMenu = document.getElementById('strawberry-menu')
+let ChocolateMenu = document.getElementById('chocolate-menu')
+let MainImage = document.getElementById('main-image')
+
 const themeSwitch = document.getElementById('theme-switch')
 
 const enableChocolatemode = () => {
     document.body.classList.add('chocolatemode')
-    localStorage.setItem('chocolatemode', 'active')
+    StrawberryMenu.classList.add('hidden')
+    ChocolateMenu.classList.remove('hidden')
+    MainImage.src = "./Pictures/Main/CCmain.jpg"
+    document.cookie = "chocolatemode=active"
 }
 
 const disableChocolatemode = () => {
-    document.body.classList.add('chocolatemode')
-    localStorage.setItem('chocolatemode', null)
+    document.body.classList.remove('chocolatemode')
+    ChocolateMenu.classList.add('hidden')
+    StrawberryMenu.classList.remove('hidden')
+    MainImage.src = "./Pictures/Main/SBmain.jpg"
+    document.cookie = "chocolatemode=null"
 }
 
-if(chocolatemode === "active") enableChocolatemode()
+if(chocolatemode === "active") 
+    enableChocolatemode() 
+else 
+    disableChocolatemode()
 
 themeSwitch.addEventListener("click", () => {
-    chocolatemode = localStorag.getItem('chocolatemode')
+    chocolatemode = getCookie('chocolatemode')
     chocolatemode !== "active" ? enableChocolatemode() : disableChocolatemode()
 })
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
